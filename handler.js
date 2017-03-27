f3af5ee78a8afb6e223014c8a50ac28922ede086
sluceno <sluceno.a@gmail.com>
2017-03-27 11:18:54 +0200

    Add showUser function.

--- handler.js
+++ handler.js
@@ -1,16 +1,31 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+const AWS = require('aws-sdk');
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+module.exports.showUser = (event, context, callback) => {
+  const params = {
+    TableName: "users",
+    Key: {
+          id: event.pathParameters.id,
+        },
   };
 
-  callback(null, response);
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  dynamoDb.get(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(new Error('Couldn\'t fetch the todo item.'));
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Item),
+    };
+
+    callback(null, response);
+  });
 };
